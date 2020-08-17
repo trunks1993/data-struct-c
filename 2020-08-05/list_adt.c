588d52a1f05c9b41aef664258bc341320d3af84c
unknown <604723443@qq.com>
2020-08-17 23:56:11 +0800

    init commit

---
+++ 2020-08-05/list_adt.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "list.h"
+
+struct Node
+{
+    ElementType Element;
+    Position Next;
+};
+
+int IsEmpty(List L)
+{
+    return L->Next == NULL;
+}
+
+int isLast(Position P, List L)
+{
+    return P->Next == NULL;
+}
+
+Position Find(ElementType X, List L)
+{
+    Position P;
+
+    P = L->Next;
+    while (P != NULL && P->Element != X)
+    {
+        P = P->Next;
+    }
+    return P;
+}
+
+void Delete(ElementType X, List L)
+{
+    Position P;
+    while (P != NULL && P->Element != X)
+    {
+        P = P->Next;
+    }
+}
+
+Position FindPrevious(ElementType X, List L)
+{
+    Position P = L;
+    while (P->Next && P->Next->Element != X)
+        P = P->Next;
+    return P;
+}
+
+// void Insert(ElementType X, List L, Position P);
+// void DeleteList(List L);
+// Position Header(List L);
+// Position First(List L);
+// Position Advance(Position P);
+// ElementType Retrieve(Position P);
+
+int main(void)
+{
+    return 0;
+}
